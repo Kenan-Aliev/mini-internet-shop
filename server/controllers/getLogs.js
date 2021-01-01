974493d3db1e495e9899965a5876e01ab9b12a94
Kenan2112 <Kenan199948@gmail.com>
2021-01-01 18:52:54 +0600

    connected mongo db

--- server/controllers/getLogs.js
+++
@@ -1,7 +0,0 @@
-import { rFile } from '../functions/readFile'
-
-// eslint-disable-next-line import/prefer-default-export
-export const getLogs = async (req, res) => {
-  const logs = await rFile('logs')
-  res.json(logs)
-}
--- server/functions/readFile.js
+++
@@ -1,7 +0,0 @@
-const { readFile } = require('fs').promises
-
-exports.rFile = (fileName) => {
-  return readFile(`server/${fileName}.json`, { encoding: 'utf8' })
-    .then((result) => JSON.parse(result))
-    .catch((err) => console.log('READ FILE ERROR', err.message))
-}
